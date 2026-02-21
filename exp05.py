27af76bab66622ba5dff50f2668e2183c139d1cf
karthikp8521 <karthikp8521@gmail.com>
2026-02-21 14:37:07 +0530

    Update exp05.py

--- exp05.py
+++ exp05.py
@@ -1,32 +1,24 @@
-# Read file and count word frequency
+from collections import Counter
 
 try:
-    # Open input file
-    with open("Python/input.txt", "r") as file:
-        text = file.read().lower()
+    with open("test.txt") as f:
+        text = f.read().lower()
 
-    # Remove punctuation
     for ch in ",.!?;:\n":
         text = text.replace(ch, " ")
 
-    words = text.split()
+    freq = Counter(text.split())
 
-    # Count frequency
-    freq = {}
-    for word in words:
-        freq[word] = freq.get(word, 0) + 1
-
-    # Display result
     print("\nWord Frequency:")
-    for word, count in freq.items():
-        print(word, ":", count)
+    for w, c in freq.items():
+        print(w, ":", c)
 
-    # Save to output file
     with open("output.txt", "w") as out:
-        for word, count in freq.items():
-            out.write(f"{word} : {count}\n")
+        for w, c in freq.items():
+            out.write(f"{w} : {c}\n")
 
     print("\nResults saved to output.txt")
 
 except FileNotFoundError:
     print("Input file not found. Please create 'input.txt'")
+
